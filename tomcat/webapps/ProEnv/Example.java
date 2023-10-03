09d08f6b78ae7cf2aacbd73e8453a4810694d34c
137braces <hayata07130916@gmail.com>
2023-10-03 21:25:47 +0900

    生年月日を年齢に変換するメソッド、試作完成

---
+++ tomcat/webapps/ProEnv/Example.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Example {
+    public static String getAge (String birthday){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/d");
+            LocalDate localBirdhdate = LocalDate.parse(birthday, formatter);
+            LocalDate nowDate = LocalDate.now();
+            return Long.toString(ChronoUnit.YEARS.between(localBirdhdate, nowDate));
+    }
+
+    public static void main(String[] args) {
+        String age = getAge("1996/7/13");
+        System.out.println(age);
+
+    }
+
+}
